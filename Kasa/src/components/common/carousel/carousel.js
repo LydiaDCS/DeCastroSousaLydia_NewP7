a5606f590679ff41df42e4fa6efb7734991e0a92
Lydia De Castro Sousa <lydiadecastrosousa@gmail.com>
2023-10-02 19:43:32 +0200

    P7 Terminé

--- Kasa/src/components/common/carousel/carousel.js
+++ Kasa/src/components/common/carousel/carousel.js
@@ -7,24 +7,22 @@ import suivantIcon from '../../../assets/suivant.svg';
 
 function Carousel({ images }) {
 
-  // Déclaration d'un état local pour stocker l'indice de l'image actuellement affichée, qui est initialisé à 0.
+  // Etat local pour stocker l'indice de l'image affichée, initialisé à 0.
   const [currentImageIndex, setcurrentImageIndex] = useState(0);
 
   // Fonction pour afficher l'image suivante dans le carrousel
   const suivanteImage = () => {
-
-    // Si on est arrivé à la fin du tableau, on retourne à l'indice 0, sinon on incrémente simplement l'indice de 1
+    // Si on est arrivé à la fin du tableau (dernière image), on retourne à l'indice 0, sinon on incrémente simplement l'indice de 1
     setcurrentImageIndex(currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1);
   };
 
   // Fonction pour afficher l'image précédente dans le carrousel
   const precedenteImage = () => {
-
-    // Si on est au début du tableau, on retourne à l'indice de la dernière image, sinon on décrémente simplement l'indice de 1
+    // Si on est au début du tableau (1ère image), on retourne à l'indice de la dernière image du tableau, sinon on décrémente simplement l'indice de 1
     setcurrentImageIndex(currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1);
   };
 
-  //Condition s'il y a plus d'une image alors affichage des buttons precedent et suivant pour defiler les images
+  //Condition s'il y a plus d'une image alors affichage des buttons precedent et suivant pour defiler les images sinon pas de buttons
 
   return (
     <div className="carousel">
